a05ca401397277751ebab2eab3801d2a8f891776
yanal97 <46229907+yanal97@users.noreply.github.com>
2019-02-14 02:31:11 +0200

    Add files via upload

---
+++ build a portfolio edited/JS/javascript.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
+
+menu.addEventListener('click', function(e) {
+drawer.classList.toggle('open');
+e.stopPropagation();
+  });
+main.addEventListener('click', function() {
+drawer.classList.remove('open');
+  });
